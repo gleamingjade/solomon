4840c846500291ba7b6a627bb64aa0985f8ba9f6
JINHYEOK PARK <pjhneverdie@gmail.com>
2026-06-27 08:44:03 +0000

    first commit

---
+++ solomon/src/main/java/com/example/solomon/SolomonApplication.java
@@ -0,0 +1,13 @@
+package com.example.solomon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolomonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolomonApplication.class, args);
+	}
+
+}
---
+++ solomon/src/test/java/com/example/solomon/SolomonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.solomon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolomonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
