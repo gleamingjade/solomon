30bc2906dea0651f9ccac44633c4821f38dcf915
JINHYEOK PARK <pjhneverdie@gmail.com>
2026-07-01 10:30:34 +0000

    chore: trial_member 테이블에 idx_member_id 인덱스 추가 외 1건
    1. trial_member 테이블에 idx_member_id 인덱스 추가
    2. trial 테이블 last_message_seq 컬럼 추가

--- solomon/src/main/resources/db/V1.02__create_table_trial.sql
+++ solomon/src/main/resources/db/V1.02__create_table_trial.sql
@@ -2,6 +2,12 @@ CREATE TABLE IF NOT EXISTS trial (
     id BINARY(16) PRIMARY KEY,
     issue_title VARCHAR(255) NOT NULL,
     stage VARCHAR(255) NOT NULL,
+
+    last_message TEXT,
+    last_message_seq INT,
+
     created_at DATETIME NOT NULL,
-    updated_at DATETIME NOT NULL
-)
+    updated_at DATETIME NOT NULL,
+
+    INDEX idx_updated_at (updated_at DESC)
+);
--- solomon/src/main/resources/db/V1.03__create_table_trial_member.sql
+++ solomon/src/main/resources/db/V1.03__create_table_trial_member.sql
@@ -3,10 +3,16 @@ CREATE TABLE IF NOT EXISTS trial_member (
     member_id INT NOT NULL,
     nickname VARCHAR(255) NOT NULL,
     turn VARCHAR(255) NOT NULL,
+
+    read_seq INT,
+    
     created_at DATETIME NOT NULL,
     updated_at DATETIME NOT NULL,
     
     PRIMARY KEY (trial_id, member_id),
+
+    INDEX idx_member_id (member_id),
+
     CONSTRAINT fk_trial_member_trial FOREIGN KEY (trial_id) REFERENCES trial (id) ON DELETE CASCADE,
     CONSTRAINT fk_trial_member_member FOREIGN KEY (member_id) REFERENCES member (id) ON DELETE CASCADE
 );
